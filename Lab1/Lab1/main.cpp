a1e9bbc210c60061fe157b45e88fad5288b430b0
Egor <etkachev398@gmail.com>
2024-09-14 11:33:34 +0300

    Fixed for floats

--- Lab1/Lab1/main.cpp
+++ Lab1/Lab1/main.cpp
@@ -4,13 +4,13 @@
 
 using namespace std::chrono;
 
-const int MATRIX_SIZE = sqrt(16 * 1024 * 1024 / sizeof(int));
-const int BLOCK_SIZE = sqrt(4 * 1024 * 1024 / sizeof(int));
+const int MATRIX_SIZE = sqrt(16 * 1024 * 1024 / sizeof(float));
+const int BLOCK_SIZE = sqrt(4 * 1024 * 1024 / sizeof(float));
 
 //const int MATRIX_SIZE = 4;
 //const int BLOCK_SIZE = 2;
 
-void clean_memory(int** matrix, int size)
+void clean_memory(float** matrix, int size)
 {
 	for (int i = 0; i < size; i++)
 	{
@@ -20,12 +20,12 @@ void clean_memory(int** matrix, int size)
 	delete[] matrix;
 }
 
-int** generate_matrix()
+float** generate_matrix()
 {
-	int** matrix = new int* [MATRIX_SIZE];
+	float** matrix = new float* [MATRIX_SIZE];
 	for (int i = 0; i < MATRIX_SIZE; i++)
 	{
-		matrix[i] = new int[MATRIX_SIZE];
+		matrix[i] = new float[MATRIX_SIZE];
 		for (int j = 0; j < MATRIX_SIZE; j++)
 		{
 			matrix[i][j] = (int)(rand() % 10);
@@ -35,7 +35,7 @@ int** generate_matrix()
 	return matrix;
 }
 
-void print_matrix(int** matrix)
+void print_matrix(float** matrix)
 {
 	for (int i = 0; i < MATRIX_SIZE; i++)
 	{
@@ -48,13 +48,13 @@ void print_matrix(int** matrix)
 	}
 }
 
-void matrix_multiplication_1(int** matrix_1, int** matrix_2, int** result)
+void matrix_multiplication_1(float** matrix_1, float** matrix_2, float** result)
 {
 	for (int i = 0; i < MATRIX_SIZE; i++)
 	{
 		for (int j = 0; j < MATRIX_SIZE; j++)
 		{
-			int result_elem = 0.0f;
+			float result_elem = 0.0f;
 			for (int k = 0; k < MATRIX_SIZE; k++)
 			{
 				result_elem += matrix_1[i][k] * matrix_2[k][j];
@@ -65,13 +65,13 @@ void matrix_multiplication_1(int** matrix_1, int** matrix_2, int** result)
 	}
 }
 
-void blocks_handling(int** matrix_1, int** matrix_2, int** result, int matrix_1_row_offset, int matrix_1_column_offset, int matrix_2_row_offset, int matrix_2_column_offset, int result_row_offset, int result_column_offset)
+void blocks_handling(float** matrix_1, float** matrix_2, float** result, int matrix_1_row_offset, int matrix_1_column_offset, int matrix_2_row_offset, int matrix_2_column_offset, int result_row_offset, int result_column_offset)
 {
 	for (int i = 0; i < BLOCK_SIZE; i++)
 	{
 		for (int j = 0; j < BLOCK_SIZE; j++)
 		{
-			int result_elem = 0.0f;
+			float result_elem = 0.0f;
 			for (int k = 0; k < BLOCK_SIZE; k++)
 			{
 				result_elem += matrix_1[i + matrix_1_row_offset][k + matrix_1_column_offset] * matrix_2[k + matrix_2_row_offset][j + matrix_2_column_offset];
@@ -82,7 +82,7 @@ void blocks_handling(int** matrix_1, int** matrix_2, int** result, int matrix_1_
 	}
 }
 
-void matrix_multiplication_2(int** matrix_1, int** matrix_2, int** result)
+void matrix_multiplication_2(float** matrix_1, float** matrix_2, float** result)
 {
 	for (int i = 0; i < MATRIX_SIZE; i += BLOCK_SIZE)
 	{
@@ -103,13 +103,13 @@ void time_calculation(high_resolution_clock::time_point start)
 	std::cout << "Took " << time_span.count() << " seconds." << std::endl;
 }
 
-bool matrix_compare(int** matrix_1, int** matrix_2)
+bool matrix_compare(float** matrix_1, float** matrix_2)
 {
 	for (int i = 0; i < MATRIX_SIZE; i++)
 	{
 		for (int j = 0; j < MATRIX_SIZE; j++)
 		{
-			if (matrix_1[i][j] != matrix_2[i][j])
+			if (abs(matrix_1[i][j] - matrix_2[i][j]) > 1e-3)
 			{
 				std::cout << matrix_1[i][j] << " " << matrix_2[i][j] << i << " " << j << std::endl;
 				return false;
@@ -123,30 +123,23 @@ bool matrix_compare(int** matrix_1, int** matrix_2)
 int main()
 {
 	srand(time(NULL));
-	int** matrix_1 = generate_matrix();
-	int** matrix_2 = generate_matrix();
-	int** result_1 = new int* [MATRIX_SIZE];
-	int** result_2 = new int* [MATRIX_SIZE];
+	float** matrix_1 = generate_matrix();
+	float** matrix_2 = generate_matrix();
+	float** result_1 = new float* [MATRIX_SIZE];
+	float** result_2 = new float* [MATRIX_SIZE];
 	for (int i = 0; i < MATRIX_SIZE; i++)
 	{
-		result_1[i] = new int[MATRIX_SIZE];
-		result_2[i] = new int[MATRIX_SIZE];
+		result_1[i] = new float[MATRIX_SIZE];
+		result_2[i] = new float[MATRIX_SIZE];
 		for (int j = 0; j < MATRIX_SIZE; j++)
 		{
 			result_1[i][j] = result_2[i][j] = 0;
 		}
 	}
 
-	//print_matrix(matrix_1);
-	std::cout << "-------------------\n";
-	//print_matrix(matrix_2);
-	std::cout << "-------------------\n";
 	high_resolution_clock::time_point start = high_resolution_clock::now();
 	matrix_multiplication_1(matrix_1, matrix_2, result_1);
 	time_calculation(start);
-	//print_matrix(result);
-	std::cout << "-------------------\n";
-
 	start = high_resolution_clock::now();
 	matrix_multiplication_2(matrix_1, matrix_2, result_2);
 	time_calculation(start);
@@ -158,6 +151,6 @@ int main()
 	{
 		std::cout << "Results aren't same." << std::endl;
 	}
-	//print_matrix(result);
+	
 	return 0;
 }
