111f02fd9716dcfd4047d2157afc3c867924c544
Dubster <shevc9@mail.ru>
2024-09-30 13:00:13 +0300

    calculation with AVX2 and parallel handling were added

--- Lab1/Lab1/main.cpp
+++ Lab1/Lab1/main.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <intrin.h>
+#include <malloc.h>
+#include "omp.h"
 
 using namespace std::chrono;
 
 const int MATRIX_SIZE = sqrt(16 * 1024 * 1024 / sizeof(float));
 const int BLOCK_SIZE = sqrt(4 * 1024 * 1024 / sizeof(float));
 
-//const int MATRIX_SIZE = 4;
-//const int BLOCK_SIZE = 2;
+//const int MATRIX_SIZE = 16;
+//const int BLOCK_SIZE = 16;
 
 void clean_memory(float** matrix, int size)
 {
 	for (int i = 0; i < size; i++)
 	{
-		delete matrix[i];
+		//delete matrix[i];
+		_aligned_free(matrix[i]);
 	}
 
-	delete[] matrix;
+	//delete[] matrix;
+	_aligned_free(matrix);
 }
 
 float** generate_matrix()
 {
-	float** matrix = new float* [MATRIX_SIZE];
+	//float** matrix = new float* [MATRIX_SIZE];
+	float** matrix = (float**)_aligned_malloc(MATRIX_SIZE * sizeof(float*), 32);
 	for (int i = 0; i < MATRIX_SIZE; i++)
 	{
-		matrix[i] = new float[MATRIX_SIZE];
+		//matrix[i] = new float[MATRIX_SIZE];
+		matrix[i] = (float*)_aligned_malloc(MATRIX_SIZE * sizeof(float), 32);
 		for (int j = 0; j < MATRIX_SIZE; j++)
 		{
 			matrix[i][j] = (int)(rand() % 10);
@@ -46,6 +53,7 @@ void print_matrix(float** matrix)
 
 		std::cout << '\n';
 	}
+	std::cout << '\n';
 }
 
 void matrix_multiplication_1(float** matrix_1, float** matrix_2, float** result)
@@ -76,25 +84,58 @@ void matrix_multiplication_1(float** matrix_1, float** matrix_2, float** result)
 	}
 }
 
-void blocks_handling(float** matrix_1, float** matrix_2, float** result, int matrix_1_row_offset, int matrix_1_column_offset, int matrix_2_row_offset, int matrix_2_column_offset, int result_row_offset, int result_column_offset)
+void blocks_parallel_handling(float** matrix_1, float** matrix_2, float** result,
+	int matrix_1_row_offset, int matrix_1_column_offset, int matrix_2_row_offset, int matrix_2_column_offset,
+	int result_row_offset, int result_column_offset)
 {
+#pragma omp parallel for
 	for (int i = 0; i < BLOCK_SIZE; i++)
 	{
 		for (int j = 0; j < BLOCK_SIZE; j++)
 		{
-			float result_elem = 0.0f;
-			for (int k = 0; k < BLOCK_SIZE; k++)
+			for (int k = 0; k < BLOCK_SIZE; k += 8)
 			{
-				//result_elem += matrix_1[i + matrix_1_row_offset][k + matrix_1_column_offset] * matrix_2[k + matrix_2_row_offset][j + matrix_2_column_offset];
-				result[j + result_row_offset][k + result_column_offset] += matrix_1[j + matrix_1_row_offset][i + matrix_1_column_offset] * matrix_2[i + matrix_2_row_offset][k + matrix_2_column_offset];
-			}
+				__m256 a = _mm256_set1_ps(matrix_1[i + matrix_1_row_offset][j + matrix_1_column_offset]);
+				__m256 b = _mm256_load_ps(&matrix_2[j + matrix_2_row_offset][k + matrix_2_column_offset]);
 
-			
+				__m256 partialResult = _mm256_load_ps(&result[i + result_row_offset][k + result_column_offset]);
+				partialResult = _mm256_fmadd_ps(a, b, partialResult);
+				_mm256_store_ps(&result[i + result_row_offset][k + result_column_offset], partialResult);
+			}
 		}
 	}
 }
 
-void matrix_multiplication_2(float** matrix_1, float** matrix_2, float** result)
+void blocks_handling(float** matrix_1, float** matrix_2, float** result,
+	int matrix_1_row_offset, int matrix_1_column_offset, int matrix_2_row_offset, int matrix_2_column_offset,
+	int result_row_offset, int result_column_offset, bool is_AVX_used)
+{
+	__m256 a, b;
+	for (int i = 0; i < BLOCK_SIZE; i++)
+	{
+		for (int j = 0; j < BLOCK_SIZE; j++)
+		{
+			for (int k = 0; k < BLOCK_SIZE; k++)
+			{
+				if (is_AVX_used)
+				{
+					a = _mm256_set1_ps(matrix_1[i + matrix_1_row_offset][j + matrix_1_column_offset]);
+					b = _mm256_load_ps(&matrix_2[j + matrix_2_row_offset][k + matrix_2_column_offset]);
+
+					__m256 partialResult = _mm256_load_ps(&result[i + result_row_offset][k + result_column_offset]);
+					partialResult = _mm256_fmadd_ps(a, b, partialResult);
+					_mm256_store_ps(&result[i + result_row_offset][k + result_column_offset], partialResult);
+
+					k += 7;
+				}
+				else
+					result[j + result_row_offset][k + result_column_offset] += matrix_1[j + matrix_1_row_offset][i + matrix_1_column_offset] * matrix_2[i + matrix_2_row_offset][k + matrix_2_column_offset];
+			}
+		}
+	}
+}
+
+void matrix_multiplication_2(float** matrix_1, float** matrix_2, float** result, bool is_AVX_used = false, bool is_parallel = false)
 {
 	for (int i = 0; i < MATRIX_SIZE; i += BLOCK_SIZE)
 	{
@@ -102,7 +143,10 @@ void matrix_multiplication_2(float** matrix_1, float** matrix_2, float** result)
 		{
 			for (int k = 0; k < MATRIX_SIZE; k += BLOCK_SIZE)
 			{
-				blocks_handling(matrix_1, matrix_2, result, i, k, k, j, i, j);
+				if (is_parallel)
+					blocks_parallel_handling(matrix_1, matrix_2, result, i, k, k, j, i, j);
+				else
+					blocks_handling(matrix_1, matrix_2, result, i, k, k, j, i, j, is_AVX_used);
 			}
 		}
 	}
@@ -134,28 +178,41 @@ bool matrix_compare(float** matrix_1, float** matrix_2)
 
 int main()
 {
-	srand(time(NULL));
+	//srand(time(NULL));
 	float** matrix_1 = generate_matrix();
 	float** matrix_2 = generate_matrix();
-	float** result_1 = new float* [MATRIX_SIZE];
-	float** result_2 = new float* [MATRIX_SIZE];
+
+	float** result_1 = (float**)_aligned_malloc(MATRIX_SIZE * sizeof(float*), 32);
+	float** result_2 = (float**)_aligned_malloc(MATRIX_SIZE * sizeof(float*), 32);
+	float** result_3 = (float**)_aligned_malloc(MATRIX_SIZE * sizeof(float*), 32);
+
 	for (int i = 0; i < MATRIX_SIZE; i++)
 	{
-		result_1[i] = new float[MATRIX_SIZE];
-		result_2[i] = new float[MATRIX_SIZE];
+		result_1[i] = (float*)_aligned_malloc(MATRIX_SIZE * sizeof(float), 32);
+		result_2[i] = (float*)_aligned_malloc(MATRIX_SIZE * sizeof(float), 32);
+		result_3[i] = (float*)_aligned_malloc(MATRIX_SIZE * sizeof(float), 32);
 		for (int j = 0; j < MATRIX_SIZE; j++)
 		{
-			result_1[i][j] = result_2[i][j] = 0;
+			result_1[i][j] = result_2[i][j] = result_3[i][j] = 0;
 		}
 	}
-
 	high_resolution_clock::time_point start = high_resolution_clock::now();
-	matrix_multiplication_1(matrix_1, matrix_2, result_1);
+	matrix_multiplication_2(matrix_1, matrix_2, result_1);
 	time_calculation(start);
+
 	start = high_resolution_clock::now();
-	matrix_multiplication_2(matrix_1, matrix_2, result_2);
+	matrix_multiplication_2(matrix_1, matrix_2, result_2, true);
 	time_calculation(start);
-	if (matrix_compare(result_1, result_2))
+
+	start = high_resolution_clock::now();
+	matrix_multiplication_2(matrix_1, matrix_2, result_3, true, true);
+	time_calculation(start);
+
+	/*print_matrix(result_1);
+	print_matrix(result_2);
+	print_matrix(result_3);*/
+
+	if (matrix_compare(result_1, result_2) && matrix_compare(result_2, result_3))
 	{
 		std::cout << "Results are same." << std::endl;
 	}
@@ -163,6 +220,6 @@ int main()
 	{
 		std::cout << "Results aren't same." << std::endl;
 	}
-	
+
 	return 0;
 }
